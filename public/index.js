e56c2354eebe315cf179acd780dc6cde59ec70b1
abhishek19994 <abhishek17005@iiitd.ac.in>
2018-06-28 19:05:00 +0530

    added templates

--- public/index.js
+++ public/index.js
@@ -9,21 +9,28 @@ socket.on('newEmail',function(data){
 	console.log(data);
 });
 socket.on('newLocationMessage',function(data){
-	li=jQuery('<li></li>');
+	/*li=jQuery('<li></li>');
 	li.text(data.from+' '+moment(data.createdAt).format('hh:mm a')+':');
 	a=jQuery('<a target="_blank">My Current location</a>')
 	a.attr('href','https://www.google.com/maps?q='+data.latitude+','+data.longitude)
 	li.append(a);
-	jQuery('#item').append(li);
+	jQuery('#messages').append(li);
 
-	console.log(data);
+	console.log(data);*/
+	var template=jQuery('#location-message-template').html();
+	var url='https://www.google.com/maps?q='+data.latitude+','+data.longitude;
+	var html=Mustache.render(template,{from:data.from,createdAt:moment(data.createdAt).format('hh:mm a'),url:url})
+	jQuery('#messages').append(html)
 })
 
 socket.on('newMessage',function(data){
-li=jQuery('<li></li>');
+/*li=jQuery('<li></li>');
 li.text(data.from+' '+moment(data.createdAt).format('hh:mm a')+' '+':'+data.text);
 jQuery('#item').append(li);
-	console.log('NewMessage',data);
+	console.log('NewMessage',data);*/
+var template=jQuery('#message-template').html();
+var html=Mustache.render(template,{text:data.text,from:data.from,createdAt:moment(data.createdAt).format('hh:mm a')})
+jQuery('#messages').append(html)
 })
 /*socket.emit('createMessage',{from :'abhi', text:'hety'},function(){console.log('got it');})*/
 jQuery('#kaki').on('submit',(e)=>{
